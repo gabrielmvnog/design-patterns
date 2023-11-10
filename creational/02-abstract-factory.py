1fd9dce346283180d52dd561055f72c709b495e6
Gabriel Nogueira <gabrielmvnogueira@gmail.com>
2023-11-10 18:01:28 -0300

    feat: add Abstract Factory

--- creational/02-abstract-factory.py
+++ creational/02-abstract-factory.py
@@ -18,9 +18,29 @@ class DwarfShield(ABC):
         print("Blocked with dwarf shield!")
 
 
+class Sword(ABC):
+    @abstractmethod
+    def attack(self: Self):
+        pass
+
+
+class ElfSword(ABC):
+    def attack(self: Self):
+        print("Attacked with elf shield!")
+
+
+class DwarfSword(ABC):
+    def attack(self: Self):
+        print("Attacked with dwarf shield!")
+
+
 class Blacksmith(ABC):
     @abstractmethod
     def sell_shield(self: Self) -> Shield:
+        pass
+
+    @abstractmethod
+    def sell_sword(self: Self) -> Shield:
         pass
 
 
@@ -28,16 +48,24 @@ class ElfBlacksmith(Blacksmith):
     def sell_shield(self: Self) -> Shield:
         return ElfShield()
 
+    def sell_sword(self: Self) -> Shield:
+        return ElfSword()
+
 
 class DwarfBlacksmith(Blacksmith):
     def sell_shield(self: Self) -> Shield:
         return DwarfShield()
 
+    def sell_sword(self: Self) -> Sword:
+        return DwarfSword()
+
 
 def buy_and_test(blacksmith: Blacksmith):
     shield: Shield = blacksmith.sell_shield()
+    sword: Sword = blacksmith.sell_sword()
 
     shield.block()
+    sword.attack()
 
 
 if __name__ == "__main__":
