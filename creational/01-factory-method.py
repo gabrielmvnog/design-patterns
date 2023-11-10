0c13a4b47e7c4eb5251e49cbd1e024fe8178128b
Gabriel Nogueira <gabrielmvnogueira@gmail.com>
2023-11-10 17:52:16 -0300

    feat: add creational Factory Method

--- creational/01-factory-method.py
+++ creational/01-factory-method.py
@@ -2,8 +2,45 @@ from abc import ABC, abstractmethod
 from typing import Self
 
 
-class Creator(ABC):
+class Shield(ABC):
     @abstractmethod
-    def create(self: Self):
-        return ...
-    
+    def equip(self: Self):
+        pass
+
+
+class ElfShield(ABC):
+    def equip(self: Self):
+        print("Equiped a elf shield!")
+
+
+class DwarfShield(ABC):
+    def equip(self: Self):
+        print("Equiped a dwarf shield!")
+
+
+class Blacksmith(ABC):
+    @abstractmethod
+    def sell_shield(self: Self) -> Shield:
+        pass
+
+
+class ElfBlacksmith(Blacksmith):
+    def sell_shield(self: Self) -> Shield:
+        return ElfShield()
+
+
+class DwarfBlacksmith(Blacksmith):
+    def sell_shield(self: Self) -> Shield:
+        return DwarfShield()
+
+
+def buy_shield(blacksmith: Blacksmith):
+    return blacksmith.sell_shield()
+
+
+if __name__ == "__main__":
+    shield: Shield = buy_shield(ElfBlacksmith())
+    shield.equip()
+
+    shield: Shield = buy_shield(DwarfBlacksmith())
+    shield.equip()
